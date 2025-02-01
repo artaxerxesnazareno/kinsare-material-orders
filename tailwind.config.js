0e016f1105a8e5d27f4fa0df56f7ef4b39e597c8
= <artaxerxesnazare@hotmail.com>
2025-02-01 07:27:01 +0100

    Upgrade dev dependencies and update configuration files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,13 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "./public/index.html"
     ],
 
     theme: {
